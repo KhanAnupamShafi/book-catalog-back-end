135d9e3ef5fcc55113270189918a34386a5a95eb
KhanAnupamShafi <deep71.bd@gmail.com>
2023-09-01 20:22:08 +0600

    lint fix

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ let server: Server;
 async function bootstrap() {
   try {
     server = app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`);
+      console.log(`Application is listening on port ${config.port}`);
     });
   } catch (err) {
     console.error('Failed to connect database', err);
