c3406f8f47b9382525d2251955ec87b4a63b6cbf
KhanAnupamShafi <deep71.bd@gmail.com>
2023-09-01 20:20:36 +0600

    lint fix

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-console */
-import { Server } from 'http'
-import app from './app'
-import config from './config/index'
+import { Server } from 'http';
+import app from './app';
+import config from './config/index';
 
+//  "**/*.{js,jsx,ts,tsx}":[
+//       "npx prettier --write",
+//       "npx eslint --fix",
+//     ],
 
 let server: Server;
 
 async function bootstrap() {
   try {
     server = app.listen(config.port, () => {
-      console.log(`Application  listening on port ${config.port}`)
-    })
+      console.log(`Application  listening on port ${config.port}`);
+    });
   } catch (err) {
-    console.error('Failed to connect database', err)
+    console.error('Failed to connect database', err);
   }
-
 }
 
-bootstrap()
+bootstrap();
